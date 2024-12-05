fd2ebf97cb794dff440227be2314d38f4f3bba1f
Thomas Warrier <thomas.warrier@etu.univ-lyon1.fr>
2024-12-05 23:35:19 +0100

    Vague

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,33 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import OceanLayer1 from "./components/OceanLayer1.vue";
+const items = [
+  {
+    color: "red-lighten-2",
+    icon: "mdi-star",
+  },
+  {
+    color: "purple-lighten-2",
+    icon: "mdi-book-variant",
+  },
+  {
+    color: "green-lighten-1",
+    icon: "mdi-airballoon",
+  },
+  {
+    color: "indigo-lighten-2",
+    icon: "mdi-layers-triple",
+  },
+];
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <OceanLayer1></OceanLayer1>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
